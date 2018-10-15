54810a6a6e5bbc3c0d04dc876a8676cf184bdf2d
yhando24 <youcef.handoura@gmail.com>
2018-10-15 16:43:36 +0200

    modification token ajouter

--- src/fr/nouas/main/action/SignIn.java
+++ src/fr/nouas/main/action/SignIn.java
@@ -1,16 +1,13 @@
 package fr.nouas.main.action;
 
-import java.util.List;
-
 import javax.persistence.EntityManager;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Query;
 import javax.servlet.http.HttpServletRequest;
 
-import org.hibernate.transform.Transformers;
-
 import fr.nouas.beans.User;
 import fr.nouas.pojo.utils.Action;
+import fr.nouas.pojo.utils.Md5;
 import fr.nouas.utils.JpaUtil;
 
 public class SignIn extends Action {
@@ -19,6 +16,8 @@ public class SignIn extends Action {
 	@Override
     public boolean executeAction(HttpServletRequest request) {
                 
+	
+		
        // if(request.getMethod().equals("POST")) {
             
        // Recuperation des donnees utilisateur    
@@ -28,10 +27,50 @@ public class SignIn extends Action {
         String role = request.getParameter("role");
         int userId = Integer.parseInt(request.getParameter("userId"));
         */
-        	String firstname = "toto";
-            String lastname = "titi";
-            String role = "admin";
-            int userId = 1;
+
+        	String firstname = "youcef";
+            String lastname = "Handoura";
+            String role = "superAdmin";
+            int userId = 5;
+            
+            String tokenSend = request.getParameter("token");
+            
+            // creation cle decryptage
+            
+            char tokenKey [] = { 
+            		tokenSend.charAt(0),
+            		tokenSend.charAt(1),
+            		tokenSend.charAt(2),
+            		tokenSend.charAt(3)
+            };
+           
+            String key = "";
+            
+            for (int i=0; i>4; i++) {
+            	switch(tokenKey[i]) {
+            	      case 0 :
+            	      key += request.getParameter("nom");
+            	      break;
+            	      
+            	      case 1 :
+                      key += request.getParameter("prenom");
+                	  break;
+                	  
+            	      case 2 :
+                	  key += request.getParameter("type");
+                	  break;
+                	  
+            	      case 3 :
+                	  key += request.getParameter("id");
+                	  break;
+                	          
+            	}
+            };
+            Md5  tF =  new Md5(key); 
+            String tokenFind = tokenSend.substring(0, 3)+tF.codeGet();
+            
+            if(tokenSend == tokenFind) {
+        
             
             boolean redirect = false;
             
@@ -42,18 +81,19 @@ public class SignIn extends Action {
             
             EntityManager em = JpaUtil.getEntityManager();
                    
-            Query q = em.createQuery("SELECT u FROM User AS u Where u.userId=:userId"); 
-            q.setParameter("userId", userId);
+           
             
             
             try {
+            	Query q = em.createQuery("SELECT u FROM User AS u Where u.userId=:userId"); 
+                q.setParameter("userId", userId);
                 User user = (User) q.getSingleResult();
                 // on envoi lutilisateur
                 request.getSession().setAttribute("user", user);
                 request.getSession().setAttribute("userid", user.getId());
                 request.getSession().removeAttribute("usererror");
                 // on redirige sur books si la connection est ok
-                redirect = true;
+                return true;
             } catch (Exception e) {
                 // si les parametres de connections sont faux.
                 request.getSession().setAttribute("usererror", "Pseudo ou mot de passe incorrect...");
@@ -84,7 +124,9 @@ public class SignIn extends Action {
           }
         
         //}
-        return true;
+      
     }
-
+            return true;
+	}
 }
+
